5a63652d333ef2f2af7253d8d789cd3cc09cfd33
Zookegger <nguyenductrung11122004@gmail.com>
2025-04-15 14:50:29 +0700

    Add Migrations

--- Bus Station Ticket Management/Migrations/20250415075000_Add delete behaviors to Trips, Tickets and Seats.Designer.cs
+++ Bus Station Ticket Management/Migrations/20250415075000_Add delete behaviors to Trips, Tickets and Seats.Designer.cs
@@ -4,6 +4,7 @@ using Bus_Station_Ticket_Management.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bus_Station_Ticket_Management.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250415075000_Add delete behaviors to Trips, Tickets and Seats")]
+    partial class AdddeletebehaviorstoTripsTicketsandSeats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bus Station Ticket Management/Migrations/20250415075000_Add delete behaviors to Trips, Tickets and Seats.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bus_Station_Ticket_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdddeletebehaviorstoTripsTicketsandSeats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seats_Trips_TripId",
+                table: "Seats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Seats_SeatId",
+                table: "Tickets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Trips_TripId",
+                table: "Tickets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seats_Trips_TripId",
+                table: "Seats",
+                column: "TripId",
+                principalTable: "Trips",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Seats_SeatId",
+                table: "Tickets",
+                column: "SeatId",
+                principalTable: "Seats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Trips_TripId",
+                table: "Tickets",
+                column: "TripId",
+                principalTable: "Trips",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seats_Trips_TripId",
+                table: "Seats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Seats_SeatId",
+                table: "Tickets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Trips_TripId",
+                table: "Tickets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seats_Trips_TripId",
+                table: "Seats",
+                column: "TripId",
+                principalTable: "Trips",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Seats_SeatId",
+                table: "Tickets",
+                column: "SeatId",
+                principalTable: "Seats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Trips_TripId",
+                table: "Tickets",
+                column: "TripId",
+                principalTable: "Trips",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
